8f195323628dc16015426db9fcafb44ac3b0d921
codingno <codingno@gmail.com>
2020-11-05 01:21:35 +0700

    Squashed commit episode-5 branch of the following:
    commit ae8ed0a6b277ee11b799e3e75655a62c1ebe5193
    Author: codingno <codingno@gmail.com>
    Date:   Thu Nov 5 01:18:16 2020 +0700
    
        refactoring route using gorilla subrouter and method

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/codingno/go-microservices
 
 go 1.15
+
+require github.com/gorilla/mux v1.8.0 
--- handlers/products.go
+++ handlers/products.go
@@ -3,10 +3,10 @@ package handlers
 import (
 	"log"
 	"net/http"
-	"regexp"
 	"strconv"
 
 	"github.com/codingno/go-microservices/data"
+	"github.com/gorilla/mux"
 )
 
 // Products get data products
@@ -19,55 +19,9 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		p.getProducts(rw, r)
-		return
-	}
-
-	if r.Method == http.MethodPost {
-		p.addProduct(rw, r)
-		return
-	}
-
-	if r.Method == http.MethodPut {
-		// expect the id in the URI
-		p.l.Println("This is Path : ", r.URL.Path)
-		reg := regexp.MustCompile(`/([0-9]+)`)
-		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
-
-		if len(g) != 1 {
-			p.l.Println("Invalid URI more than one id")
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-
-		if len(g[0]) != 2 {
-			p.l.Println("Invalid URI more than one capture group")
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-
-		idString := g[0][1]
-		id, err := strconv.Atoi(idString)
-
-		if err != nil {
-			p.l.Println("Invalid URI unable to convert to number")
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-
-		p.updateProducts(id, rw, r)
-
-	}
-
-	// catch all
-	// if no method is satisfied return an error
-	rw.WriteHeader(http.StatusMethodNotAllowed)
-}
-
-func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
-	p.l.Println("Handle POST Product")
+// GetProducts is returning all of Products
+func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET Product")
 
 	// fetch the products from the datastore
 	lp := data.GetProducts()
@@ -82,7 +36,8 @@ func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	// rw.Write(d)
 }
 
-func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+// AddProduct is add Product
+func (p *Products) AddProduct(rw http.ResponseWriter, r *http.Request) {
 	p.l.Println("Handle POST Product")
 
 	prod := &data.Product{}
@@ -95,11 +50,19 @@ func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
 	data.AddProduct(prod)
 }
 
-func (p *Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+// UpdateProducts is making changing into  Product
+func (p *Products) UpdateProducts(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+
+	if err != nil {
+		http.Error(rw, "Unable to decoder json", http.StatusBadRequest)
+	}
+
 	p.l.Println("Handle PUT Product")
 
 	prod := &data.Product{}
-	err := prod.FromJSON(r.Body)
+	err = prod.FromJSON(r.Body)
 
 	if err != nil {
 		http.Error(rw, "Unable to decoder json", http.StatusBadRequest)
