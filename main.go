ae8ed0a6b277ee11b799e3e75655a62c1ebe5193
codingno <codingno@gmail.com>
2020-11-05 01:18:16 +0700

    refactoring route using gorilla subrouter and method

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/codingno/go-microservices/handlers"
+	"github.com/gorilla/mux"
 )
 
 func main() {
@@ -20,8 +21,18 @@ func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	ph := handlers.NewProducts(l)
 
-	sm := http.NewServeMux()
-	sm.Handle("/", ph)
+	sm := mux.NewRouter()
+
+	getRouter := sm.Methods(http.MethodGet).Subrouter()
+	getRouter.HandleFunc("/", ph.GetProducts)
+
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProducts)
+
+	postRouter := sm.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", ph.AddProduct)
+
+	// sm.Handle("/", ph)
 
 	s := &http.Server{
 		Addr:         ":" + port,
